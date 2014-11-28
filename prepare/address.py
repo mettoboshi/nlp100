2e38112639e686b2e70f17da0b215c89cda0c0fd
mettoboshi <mettoboshi.1108@gmail.com>
2014-11-28 11:30:40 +0900

    modify address.py

--- prepare/address.py
+++ prepare/address.py
@@ -12,47 +12,8 @@ def main():
   zipFileName = os.path.basename(ZIP_CODE_URL)
 #  getData(zipFileName)
   files = unZip(zipFileName)
-  print(files[0])
- 
-  PREFECTURE = 6
-  CITY = 7
-  TOWN = 8
-
-  address_set=set()
-  with codecs.open(DIR_PATH + files[0], "r", "shift_jis") as fin:
-    rows = csv.reader(fin, delimiter=",")
-    for row in rows:
-      address = "".join([row[PREFECTURE], row[CITY]])
-      town = row[TOWN]
-      if town == "以下に掲載がない場合":
-        continue
-      else:
-        sections = town.split("、") 
-        for s in sections:
-          house_number_index_en = s.find("(")
-          house_number_index_em = s.find("（")
-          if house_number_index_en > -1:
-            s = s[:house_number_index_en]
-          elif house_number_index_em > -1:
-            s = s[:house_number_index_em]
-          address_set.add((address, s))
-      
-  datasets = []
-  separator = "\t"
-  for add1, add2 in address_set:
-    datasets.append((add1, add2))
-
-  opath = DIR_PATH + FILE_NAME
-
-  with open(opath, "wb") as of:
-    for dataset in datasets:
-      if isinstance(row, list) or isinstance(row, tuple):
-        line = ""
-        line = separator.join(dataset) + "\n"
-      of.write(line.encode("utf-8"))
-
-
-
+  datasets = getDatasets(files[0]) 
+  writeFile(datasets)
 
 def getData(zipFileName):
   req = requests.get(ZIP_CODE_URL)
@@ -71,6 +32,50 @@ def unZip(zipFileName):
         unziped.write(zdir.read(zf))
 
   return files    
+
+def getDatasets(filename):
+  
+  PREFECTURE = 6
+  CITY = 7
+  TOWN = 8
+
+  address_set=set()
+  with codecs.open(DIR_PATH + filename, "r", "shift_jis") as fin:
+    rows = csv.reader(fin, delimiter=",")
+    for row in rows:
+      address = "".join([row[PREFECTURE], row[CITY]])
+      town = row[TOWN]
+      if town == "以下に掲載がない場合":
+        continue
+      else:
+        sections = town.split("、") 
+        for s in sections:
+          house_number_index_en = s.find("(")
+          house_number_index_em = s.find("（")
+          if house_number_index_en > -1:
+            s = s[:house_number_index_en]
+          elif house_number_index_em > -1:
+            s = s[:house_number_index_em]
+          address_set.add((address, s))
+      
+  datasets = []
+  for add1, add2 in address_set:
+    datasets.append((add1, add2))
+
+  return datasets
+
+def writeFile(datasets):
+  separator = "\t"
+  opath = DIR_PATH + FILE_NAME
+
+  with open(opath, "wb") as of:
+    for dataset in datasets:
+      if isinstance(dataset, list) or isinstance(dataset, tuple):
+        line = ""
+        line = separator.join(dataset) + "\n"
+        print(line)
+      of.write(line.encode("utf-8"))
+  return
 
 if __name__ == "__main__":
   main()
